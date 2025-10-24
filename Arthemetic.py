ed58e9783088f773667c24b0e36878f5a44ac549
Swapna-Karlawad <swapnakarlawad@gmail.com>
2025-10-24 11:35:19 +0530

    Create Arthemetic.py

---
+++ Arthemetic.py
@@ -0,0 +1,10 @@
+# Program to add and subtract two numbers
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# Performing addition and subtraction
+add = num1 + num2
+sub = num1 - num2
+# Displaying results
+print("Addition:", add)
+print("Subtraction:", sub)
